38e5271f1ae6df1b98902c748912816c87a73a0d
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2024-10-17 21:50:55 +0700

    Update Setting

--- Public/js/setting.js
+++ Public/js/setting.js
@@ -1,4 +1,24 @@
-document.getElementById('colorInput').addEventListener('input', function() {
-    var headingContainer = document.getElementById('headingContainer');
-    headingContainer.style.backgroundColor = this.value;
-});
+const lightBackground = "../videos/dark_tech.mp4"
+const darkBackground = "../videos/white_tech.mp4"
+const backgroundVideo = document.getElementById("backgroundVideo")
+
+// const homeBtn = document.querySelector(".home-btn")
+// homeBtn.addEventListener("click", () => {
+//     window.location.href = "main_interface.html"
+// })
+
+const theme = localStorage.getItem("theme")
+if (theme) {
+    if (theme === "dark-mode") {
+        if (!document.body.classList.contains("dark-mode")) {
+            document.body.classList.add("dark-mode")
+        }
+        backgroundVideo.src = darkBackground
+
+    } else if (theme === "light-mode") {
+        if (document.body.classList.contains("dark-mode")) {
+            document.body.classList.remove("dark-mode")
+        }
+        backgroundVideo.src = lightBackground
+    }
+}
